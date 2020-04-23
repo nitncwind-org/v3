04aaf94bcb8e323e1bc38af5b656db9931965961
tadaren <ptymaato@yahoo.co.jp>
2020-04-23 15:40:15 +0900

    ハンバーガーメニューを追加 #4

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,35 @@
 <template>
-  <v-app-bar id="header">
-    <v-btn text to="/">Home</v-btn>
-    <v-btn text to="/about">About</v-btn>
-    <v-btn text to="/concerts">Concerts</v-btn>
-    <v-btn text to="/concours">Concours</v-btn>
-    <v-btn text to="/link">Link</v-btn>
-  </v-app-bar>
+  <div>
+    <v-navigation-drawer v-model="drawer" temporary right fixed>
+      <v-list>
+        <v-list-item to="/">Home</v-list-item>
+        <v-list-item to="/about">About</v-list-item>
+        <v-list-item to="/concerts">Concerts</v-list-item>
+        <v-list-item to="/concours">Concours</v-list-item>
+        <v-list-item to="/link">Link</v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar id="header">
+      <v-btn text to="/">Home</v-btn>
+      <div class="d-none d-sm-flex">
+      <v-btn text to="/about">About</v-btn>
+      <v-btn text to="/concerts">Concerts</v-btn>
+      <v-btn text to="/concours">Concours</v-btn>
+      <v-btn text to="/link">Link</v-btn>
+      </div>
+      <v-spacer></v-spacer>
+      <v-app-bar-nav-icon class="hidden-sm-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  data: function() {
+    return {
+      drawer: null,
+    }
+  }
 }
 </script>
