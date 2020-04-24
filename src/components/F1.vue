5587a5c7cf5e865706371ca6f9e5d6c710409d85
tadaren <ptymaato@yahoo.co.jp>
2020-04-25 01:10:12 +0900

    最新コンサート情報のコンポーネントを修正
    rootを"div"から"v-card"に変更

--- src/components/F1.vue
+++ src/components/F1.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="f1">
-    <h1>{{ d.title }}</h1>
-    <div v-if="d.notice">
-      <h2>{{ d.notice.title }}</h2>
-      <p>{{ d.notice.text }}</p>
-    </div>
-    <p>日時：{{ d.date.year }}年{{ d.date.month }}月{{ d.date.day }}日</p>
-    <p>{{ d.open }}開場 {{ d.start }}開演</p>
-    <p>会場：{{ d.place }}</p>
-    <p>
-      入場料：<span v-if="d.fee===0">無料</span>
-            <span v-else>{{ d.fee }}円</span>
-    </p>
-  </div>
+  <v-card id="f1">
+    <v-card-title>{{ d.title }}</v-card-title>
+    <v-card-text>
+      <div v-if="d.notice">
+        <h2>{{ d.notice.title }}</h2>
+        <p>{{ d.notice.text }}</p>
+      </div>
+      <p>日時：{{ d.date.year }}年{{ d.date.month }}月{{ d.date.day }}日</p>
+      <p>{{ d.open }}開場 {{ d.start }}開演</p>
+      <p>会場：{{ d.place }}</p>
+      <p>
+        入場料：<span v-if="d.fee===0">無料</span>
+              <span v-else>{{ d.fee }}円</span>
+      </p>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
