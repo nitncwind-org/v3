e70d4f946a3c273656c0a7bc52354349f57e8a9c
tadaren <ptymaato@yahoo.co.jp>
2020-07-19 02:06:09 +0900

    お問い合わせフォームのデータ送信を実装　#10

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,12 +6,12 @@
         <v-form ref="form" v-model="valid">
           <v-text-field label="名前" :rules="[rules.required]" v-model="name"></v-text-field>
           <v-select label="所属" :rules="[rules.required]" :items="affiliations" v-model="affiliation"></v-select>
-          <v-text-field label="所属高専" v-if="affiliation=='他高専(OBOG含む)'"></v-text-field>
-          <v-text-field label="団体名" v-if="affiliation=='団体'"></v-text-field>
+          <v-text-field label="所属高専" v-if="affiliation=='他高専(OBOG含む)'" v-model="affiliation_kosen"></v-text-field>
+          <v-text-field label="団体名" v-if="affiliation=='団体'" v-model="affiliation_group"></v-text-field>
           <v-text-field label="メールアドレス" :rules="[rules.required, rules.email]" v-model="emailAddress"></v-text-field>
           <v-select label="お問い合わせ内容" :rules="[rules.required]" :items="inquiries" v-model="inquiry"></v-select>
           <v-textarea label="お問い合わせ内容" v-model="content"></v-textarea>
-          <v-btn :disabled="!valid">送信</v-btn>
+          <v-btn :disabled="!valid" v-on:click="submit">送信</v-btn>
         </v-form>
       </v-card-text>
     </v-card>
@@ -25,6 +25,8 @@ export default {
       valid: false,
       name: '',
       affiliation: '',
+      affiliation_kosen: '',
+      affiliation_group: '',
       emailAddress: '',
       inquiry: '',
       content: '',
@@ -45,6 +47,33 @@ export default {
         'なにか',
         'その他'
       ]
+    }
+  },
+  methods: {
+    submit(){
+      let affiliation_kosen = this.affiliation_kosen;
+      let affiliation_group = this.affiliation_group;
+      if(this.affiliation == "奈良高専(OBOG含む)" || this.affiliation == "個人"){
+        affiliation_kosen = '';
+        affiliation_group = '';
+      }
+      if(this.affiliation == "他高専(OBOG含む)"){
+        affiliation_group = '';
+      }
+      if(this.affiliation == "団体"){
+        affiliation_kosen = '';
+      }
+      let params = new URLSearchParams();
+      params.append("name", this.name);
+      params.append("affiliation", this.affiliation);
+      params.append("affiliation_kosen", affiliation_kosen);
+      params.append("affiliation_group", affiliation_group);
+      params.append("mail", this.emailAddress);
+      params.append("inquiry", this.inquiry);
+      params.append("content", this.content);
+      this.axios.post("https://script.google.com/macros/s/AKfycbxYDFF-V927nDu8dRQ4AOtWfZ--OQyccT0k9brXgE2AxCrWdqHa/exec", params).then(res => {
+        console.log(res);
+      })
     }
   }
 }
