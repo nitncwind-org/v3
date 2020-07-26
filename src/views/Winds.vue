1bda4fb7204d3b48b2ef397814bd13d94cf71c63
tadaren <ptymaato@yahoo.co.jp>
2020-07-27 00:20:20 +0900

    吹奏楽コンクール実績のページをCSVからのデータ読み込みに変更 #19

--- src/views/Winds.vue
+++ src/views/Winds.vue
@@ -14,6 +14,7 @@
 
 <script>
 import F3 from '@/components/F3.vue'
+import { loadCSV } from '@/lib/csv.js'
 
 export default {
   name: 'Winds',
@@ -28,10 +29,41 @@ export default {
     }
   },
   created() {
-    const URL = process.env.BASE_URL + 'data/concours.json';
-    this.axios.get(URL).then(res => {
-      this.concours = res.data.concours;
-
+    const URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQxQ-SUZmEy2TqWGIIHa6aFI_e3JY7lfASZwAhX3GLf-1_24RRw4xAjkDQPvF7UffSjSZfWullZSLFD/pub?gid=0&single=true&output=csv';
+    loadCSV(URL, array => {
+      let concours = {
+        'year': Number(array[0]),
+        'setPiece': {
+          'title': array[1],
+          'subtitle': array[2],
+          'composer': array[3],
+          'arranger': ''
+        },
+        'freeProgram': {
+          'title': array[4],
+          'subtitle': array[5],
+          'composer': array[6],
+          'arranger': array[7]
+        },
+        'concours': [
+          {
+            'label': '奈良県大会',
+            'award': array[9],
+            'isRepresent': false,
+          }
+        ]
+      };
+      if(array[12] !== ''){
+        concours['concours'][0]['isRepresent'] = true;
+        concours['concours'].push({
+          'label': '関西大会',
+          'award': array[12],
+          'isRepresent': false,
+        });
+      }
+      return concours;
+    }).then(res => {
+      this.concours = res.reverse();
       let m = new Map();
       this.concours.forEach(e => {
         e.generation = Math.floor(e.year / 10) * 10;
