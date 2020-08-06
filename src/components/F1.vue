8691107038faffb4f27054cfaf81d6fafece6ba3
Yu Ohno <kantillo0683@gmail.com>
2020-08-07 01:01:19 +0900

    concertコンポーネントF1のレイアウト作成

--- src/components/F1.vue
+++ src/components/F1.vue
@@ -1,18 +1,27 @@
 <template>
   <v-card tile outlined id="f1" :disabled="isEnd">
     <v-card-title class="serif text-primary mb-2 pb-0">{{ d.title }}</v-card-title>
-    <v-card-text>
-      <div v-if="d.notice">
-        <h2>{{ d.notice.title }}</h2>
-        <p>{{ d.notice.text }}</p>
+    <v-card-text class="d-flex">
+      <div id="concertInfo" >
+        <div v-if="d.notice" class="alert alert-danger" id="notice">
+          <h2 class="h6 font-weight-bold">{{ d.notice.title }}</h2>
+          <p>{{ d.notice.text }}</p>
+        </div>
+        <div id="detail">
+          <p>日時：{{ d.date.year }}年{{ d.date.month }}月{{ d.date.day }}日</p>
+          <p>{{ d.open }}開場 {{ d.start }}開演</p>
+          <p>会場：{{ d.place }}</p>
+          <p>
+            入場料：<span v-if="d.fee===0">無料</span>
+                  <span v-else>{{ d.fee }}円</span>
+          </p>
+        </div>
       </div>
-      <p>日時：{{ d.date.year }}年{{ d.date.month }}月{{ d.date.day }}日</p>
-      <p>{{ d.open }}開場 {{ d.start }}開演</p>
-      <p>会場：{{ d.place }}</p>
-      <p>
-        入場料：<span v-if="d.fee===0">無料</span>
-              <span v-else>{{ d.fee }}円</span>
-      </p>
+      <div v-if="d.poster" id="concertPoster" class="">
+        <div id="posterImage">
+          
+        </div>
+      </div>
     </v-card-text>
   </v-card>
 </template>
@@ -46,4 +55,37 @@ export default {
 .theme--light.v-card--disabled{
   background-color: #EEEEEE;
 }
+
+#concertPoster{
+  width: 210px;
+  height: 297px;
+  margin-left: 5px;
+}
+#posterImage{
+  background-color: #eee;
+  width: 100%;
+  height: 100%;
+}
+#concertInfo{
+  width: calc(100% - 200px);
+  margin-right: 5px;
+}
+
+
+#notice{
+  padding: 10px;
+  width: 75%;
+}
+
+#notice p{
+  margin-bottom: 0;
+}
+
+@media only screen and (max-width: 768px) {
+  #notice{
+    width: 100%;
+  }
+
+}
+
 </style>
