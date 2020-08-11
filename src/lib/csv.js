fb4e3fc6e8f6ca1e4b371ff888ee3a62b46d0672
tadaren <ptymaato@yahoo.co.jp>
2020-08-12 00:57:32 +0900

    コンクール実績のCSV読み込みを修正 #65

--- src/lib/csv.js
+++ src/lib/csv.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
-function loadCSV(url, convert){
-    return axios.get(url).then(res => {
+const BASE_URL = 'https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec'
+
+function loadCSV(param, convert){
+    return axios.get(BASE_URL, { params: {key: param}}).then(res => {
         return res.data.split('\n').slice(2).map(e => {
             return convert(e.split(','))
         });
