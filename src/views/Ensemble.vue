671a50c492611cee36e7f102fad174a2c7bef208
とろんぽっと.nara <NAKNAO-nnct@users.noreply.github.com>
2020-09-26 01:41:07 +0900

    Revert "Revert "Feature/design""

--- src/views/Ensemble.vue
+++ src/views/Ensemble.vue
@@ -15,6 +15,7 @@
 
 <script>
 import F3 from '@/components/F3.vue'
+import { loadCSV } from '@/lib/csv.js'
 import Title from '@/components/Title.vue'
 
 export default {
@@ -31,12 +32,44 @@ export default {
     }
   },
   created() {
-    const URL = process.env.BASE_URL + 'data/ensemble.json';
-    this.axios.get(URL).then(res => {
-      this.ensemble = res.data.ensemble;
-
+    const PARAM = 'ensemble';
+    loadCSV(PARAM, array => {
+      let ensemble = {
+        'year': Number(array[0]),
+        'music': {
+          'title': array[1],
+          'subtitle': array[2],
+          'composer': array[3],
+          'arranger': array[4]
+        },
+        'concours':[
+          {
+            'label': '奈良県大会',
+            'award': array[6],
+            'isRepresent': false
+          }
+        ]
+      };
+      if(array[9] !== ''){
+        ensemble['concours'][0]['isRepresent'] = true;
+        if(ensemble.year === 1972){
+          // 1972年は奈良県大会がないので消す
+          ensemble['concours'].pop();
+        }
+        ensemble['concours'].push({
+          'label': '関西大会',
+          'award': array[9],
+          'isRepresent': false,
+        });
+      }
+      return ensemble;
+    }).then(res => {
+      this.ensemble = res.reverse();
       let m = new Map();
       this.ensemble.forEach(e => {
+        if(isNaN(e.year)){
+          return;
+        }
         e.generation = Math.floor(e.year / 10) * 10;
         if(m.get(e.generation) === undefined){
           m.set(e.generation, []);
