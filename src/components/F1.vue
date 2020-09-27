48815443efccb23a45c7b2d674369236f652089d
NakanoYuki <totoro.windows@outlook.com>
2020-09-27 15:27:09 +0900

    fix: マージミス修正

--- src/components/F1.vue
+++ src/components/F1.vue
@@ -1,46 +1,45 @@
 <template>
   <v-card tile outlined id="f1">
-    <v-card-title class="serif text-primary mb-2 pb-0">{{ d.title }}</v-card-title>
+    
     <v-card-text class="">
-      <div v-if="isPublished" class="d-block d-lg-none alert" v-bind:class="[ d.notice.type === 'danger' ? 'alert-danger' : (d.notice.type === 'warning' ? 'alert-warning' : 'alert-info') ]"  id="notice">
-        <h2 class="h6 font-weight-bold">{{ d.notice.title }}</h2>
-        <p>{{ d.notice.text }}</p>
-      </div>
+      <v-alert v-if="isPublished" text prominent :type="d.notice.type">
+        <h3 class="title font-weight-bold">{{ d.notice.title }}</h3>
+        <div>{{ d.notice.text }}</div>
+      </v-alert>
 
-      <div class="d-flex">
-      <div id="concertInfo">
-        <div v-if="isPublished" class="d-none d-lg-block alert" :class="[ d.notice.type === 'danger' ? 'alert-danger' : (d.notice.type === 'warning' ? 'alert-warning' : 'alert-info') ]" id="notice">
-          <h2 class="h6 font-weight-bold">{{ d.notice.title }}</h2>
-          <p>{{ d.notice.text }}</p>
+      <div class="d-flex" id="concertInfo" style="column-gap: 20px; justify-content:space-between;">
+
+          <div id="detail">
+            <v-card-title class="text-primary mt-0 mb-0 pt-0 pl-0">{{ d.title }}</v-card-title>
+            <p>日時：{{ d.date.year }}年{{ d.date.month }}月{{ d.date.day }}日</p>
+            <p>{{ d.open }}開場 {{ d.start }}開演</p>
+            <p>会場：{{ d.place.name }}</p>
+            <p>
+              入場料：<span v-if="d.fee===0">無料</span>
+                    <span v-else>{{ d.fee }}円</span>
+            </p>
+          </div>
+
+      
+        
+         <div v-if="d.poster" id="concertPoster" class="">
+          <img id="posterImage" :src="d.poster">
         </div>
-        <div id="detail">
-          <p>日時：{{ d.date.year }}年{{ d.date.month }}月{{ d.date.day }}日</p>
-          <p>{{ d.open }}開場 {{ d.start }}開演</p>
-          <p>会場：{{ d.place.name }}</p>
-          <p>
-            入場料：<span v-if="d.fee===0">無料</span>
-                  <span v-else>{{ d.fee }}円</span>
-          </p>
-        </div>
-      </div>
-      <div v-if="d.poster" id="concertPoster" class="">
-        <img id="posterImage" :src="d.poster">
-      </div>
        
-    </div>
-    <div v-if="d.place.map">
-    <iframe
-              :src="d.place.map" 
-              width="100%"
-              height="300"
-              frameborder="0"
-              style="border:0"
-              allowfullscreen
-          >
-          </iframe>
-    </div>
-    <div v-html="d.mainBody">
-    </div>
+      </div>
+      <div v-if="d.place.map">
+        <iframe
+          :src="d.place.map" 
+          width="100%"
+          height="300"
+          frameborder="0"
+          style="border:0"
+          allowfullscreen
+        >
+        </iframe>
+      </div>
+      <div v-html="d.mainBody">
+      </div>
     </v-card-text>
     
   </v-card>
@@ -94,16 +93,17 @@ iframe{
    
   margin-left: 5px;
 }
+
 #posterImage{
   background-color: #eee;
   width: 100%;
   height: 100%;
 }
+
 #concertInfo{
   width: 100%;
   margin-right: 5px;
 }
-
 
 #notice{
   padding: 10px;
@@ -113,6 +113,5 @@ iframe{
 #notice p{
   margin-bottom: 0;
 }
-
 
 </style>
