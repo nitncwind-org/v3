6cc3fc1cf8914f6b77e37cb0ec66986b86dc4b19
tadaren <ptymaato@yahoo.co.jp>
2020-10-04 00:58:31 +0900

    .envファイルからURL設定を読み込むように変更 #132

--- src/config/development.js
+++
@@ -1,6 +0,0 @@
-export const ABOUT_URL = 'https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=about';
-export const CONCERTS_URL = 'https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=concerts';
-export const WINDS_URL = 'https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=winds';
-export const ENSEMBLE_URL = 'https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=ensemble';
-export const LINK_URL = 'https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=link';
-export const NEWS_URL = 'https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=news';
--- src/config/production.js
+++
@@ -1,6 +0,0 @@
-export const ABOUT_URL = 'https://nitncwind.org/view-assets/csv/about.csv';
-export const CONCERTS_URL = 'https://nitncwind.org/view-assets/csv/concerts.csv';
-export const WINDS_URL = 'https://nitncwind.org/view-assets/csv/winds.csv';
-export const ENSEMBLE_URL = 'https://nitncwind.org/view-assets/csv/ensemble.csv';
-export const LINK_URL = 'https://nitncwind.org/view-assets/csv/link.csv';
-export const NEWS_URL = 'https://nitncwind.org/view-assets/csv/news.csv';
