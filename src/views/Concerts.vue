d90c861351f6363c8cf1125ca16d9915a7b705fc
Yu Ohno <kantillo0683@gmail.com>
2020-10-06 22:11:20 +0900

    日時と演奏会名の始点の位置整列

--- src/views/Concerts.vue
+++ src/views/Concerts.vue
@@ -3,7 +3,11 @@
     <Title en="Concerts" ja="演奏会のお知らせ"></Title>
 
     <div v-for="(lc, i) in latest" :key=i :index=i id="concertList"  v-bind:class="{ended: lc.isEnd}">
-      <router-link :to="`/concerts/${lc.id}`">{{ lc.date.year }}.{{ lc.date.month }}.{{ lc.date.day }} {{ lc.title }}</router-link>
+      <v-row>
+      <v-col cols=4 md=3><span class="list_date">{{ lc.date.year }}年{{ ('0' + lc.date.month).slice(-2) }}月{{ ('0' + lc.date.day).slice(-2) }}日</span></v-col>
+      
+      <v-col cols=8 md=9><router-link :to="`/concerts/${lc.id}`"><span>{{ lc.title }}</span></router-link></v-col>
+      </v-row>
       <v-divider></v-divider>
     </div>
   </v-container>
