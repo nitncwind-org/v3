86cf94f65cb28009f8ed9b5044694f5d5ba7ca2c
Yu Ohno <kantillo0683@gmail.com>
2020-10-26 23:03:30 +0900

    fix: footer 郵便番号追加 #139

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,9 +8,9 @@
       <v-col col=12 lg=4 class="text-center d-inline text-caption">
         <span>&copy; 2014-{{ now }} 奈良高専吹奏楽部</span>
 
-        <div class="d-inline d-lg-none text-center">
+        <div class="d-inline d-lg-none text-center text-small">
           <br/>
-          <span>奈良県大和郡山市矢田町22番地 合同教室</span><br/>
+          <span>〒639-1080 奈良県大和郡山市矢田町22番地 合同教室</span><br/>
           <div class="social">
             <a href="https://twitter.com/NITNC_Band">
               <span><v-icon size="20px" class="mr-1">mdi-twitter</v-icon>@NITNC_Band</span>
@@ -50,10 +50,6 @@ export default {
   .v-footer .col{
     margin: auto 0;
   }
-
-  .v-footer{
-    
-  }
   
 </style>
 
@@ -64,6 +60,10 @@ export default {
   .v-footer .social a:hover{
     opacity: 0.7;
     text-decoration: none;
+  }
+
+  .v-footer .text-small{
+    font-size: 1rem;
   }
 
 </style>
